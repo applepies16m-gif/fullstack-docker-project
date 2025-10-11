20636d89f40b244353e8c4cbfd05c6663ed8c302
Ubuntu <ubuntu@ip-172-31-19-91.ec2.internal>
2025-10-11 14:51:34 +0000

    Add fullstack frontend-backend and nginx setup

---
+++ frontend/post/api-post.js
@@ -0,0 +1,133 @@
+const create = (params, credentials, post) => {
+  return fetch('/api/message', {       // <-- point to backend route
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json', // must stringify JSON
+      //'Authorization': 'Bearer ' + credentials.t  <-- remove for now
+    },
+    body: JSON.stringify({ text: post })  // backend expects { text: "..." }
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+const listByUser = (params, credentials) => {
+  return fetch('/api/posts/by/'+ params.userId, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    }
+  }).then(response => {
+    return response.json()
+  }).catch((err) => console.log(err))
+}
+
+const listNewsFeed = (params, credentials) => {
+  return fetch('/api/posts/feed/'+ params.userId, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    }
+  }).then(response => {
+    return response.json()
+  }).catch((err) => console.log(err))
+}
+
+const remove = (params, credentials) => {
+  return fetch('/api/posts/' + params.postId, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    }
+  }).then((response) => {
+    return response.json()
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+const like = (params, credentials, postId) => {
+  return fetch('/api/posts/like/', {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    },
+    body: JSON.stringify({userId:params.userId, postId: postId})
+  }).then((response) => {
+    return response.json()
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+const unlike = (params, credentials, postId) => {
+  return fetch('/api/posts/unlike/', {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    },
+    body: JSON.stringify({userId:params.userId, postId: postId})
+  }).then((response) => {
+    return response.json()
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+const comment = (params, credentials, postId, comment) => {
+  return fetch('/api/posts/comment/', {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    },
+    body: JSON.stringify({userId:params.userId, postId: postId, comment: comment})
+  }).then((response) => {
+    return response.json()
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+const uncomment = (params, credentials, postId, comment) => {
+  return fetch('/api/posts/uncomment/', {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    },
+    body: JSON.stringify({userId:params.userId, postId: postId, comment: comment})
+  }).then((response) => {
+    return response.json()
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+export {
+  listNewsFeed,
+  listByUser,
+  create,
+  remove,
+  like,
+  unlike,
+  comment,
+  uncomment
+}
